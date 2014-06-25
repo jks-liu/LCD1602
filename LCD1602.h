dc1b27bd9d87a0ffa3938cf3790094bb9d22e98e
Jks Liu <chinatianma@gmail.com>
2014-06-25 12:14:06 +0800

    add LCD1602.c/.h

---
+++ LCD1602.h
@@ -0,0 +1,17 @@
+#ifndef LCD1602_LCD1602_H_
+#define LCD1602_LCD1602_H_
+
+#include "software-config.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LCD1602_LCD1602_H_ */
